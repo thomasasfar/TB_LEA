1f21e863a6ddf8e28dbe8e97ba4e2e652002b8a5
Nadia Deari Hanifah <nadyadearihanifah@gmail.com>
2023-07-25 20:52:35 +0700

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -10,7 +10,7 @@
 
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
 
-    <link href={{ asset('../css/loginstyle.css') }} rel="stylesheet">
+    <link href={{ asset('/css/loginstyle.css') }} rel="stylesheet">
 </head>
 
 <body>
