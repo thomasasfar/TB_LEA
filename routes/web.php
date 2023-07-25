e9cd350c94ac12de355a49a71c88b7851afd3fd5
Nadia Deari Hanifah <nadyadearihanifah@gmail.com>
2023-07-25 15:34:04 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -20,4 +20,7 @@ Route::get('/barang', [App\Http\Controllers\BarangController::class, 'index']);
 Route::post('/barang/tambah', [App\Http\Controllers\BarangController::class, 'store']);
 Route::delete('/barang/{barang}/hapus', [App\Http\Controllers\BarangController::class, 'destroy']);
 Route::put('/barang/{barang}/update', [App\Http\Controllers\BarangController::class, 'update']);
- Route::get('/login',[AuthController::class,'login']);
+ 
+
+Route::get('/login',[AuthController::class,'login']);
+ Route::get('/register',[AuthController::class,'register']);
