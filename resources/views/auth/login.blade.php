50cafd6f501f0298f5493d640a4a9c504956bd48
Nadia Deari Hanifah <nadyadearihanifah@gmail.com>
2023-07-25 20:15:27 +0700

    .

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,79 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
-    <title>Login</title>
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
-
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700&display=swap" rel="stylesheet" />
-
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" />
-
-    <link href={{ asset('../css/loginstyle.css') }} rel="stylesheet">
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Login Form</title>
+  <!-- Link to Bootstrap CSS -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
-
 <body>
-    <section class="ftco-section">
 
-        <div class="container">
-            <div class="row justify-content-center">
-                <!-- <div class="col-md-6 text-center mb-5">
-     <h2 class="heading-section">Login #08</h2>
-    </div> -->
-            </div>
-            <div class="row justify-content-center">
-                <div class="col-md-6 col-lg-5">
-                    <div class="login-wrap p-4 p-md-5">
-                        <div class="icon d-flex align-items-center justify-content-center">
-                            <span class="fa fa-user-o"></span>
-                        </div>
-
-                        <h3 class="text-center mb-4">LOGIN</h3>
-
-                        <form action="/inventaris/login" class="login-form" method="post">
-                            @csrf
-                            <div class="form-group">
-                                <input type="email" class="form-control rounded-left" placeholder="Email "
-                                    value="{{ Session::get('email') }}" name="email" required />
-                            </div>
-                            <div class="form-group d-flex">
-                                <input type="password" class="form-control rounded-left" name="password"
-                                    placeholder="Password" required />
-                            </div>
-                            <div class="form-group d-md-flex">
-                                <!-- <div class="w-50">
-                                        <label
-                                            class="checkbox-wrap checkbox-primary"
-                                            >Remember Me
-                                            <input type="checkbox" checked />
-                                            <span class="checkmark"></span>
-                                        </label>
-                                    </div> -->
-                                <!-- <div class="w-30 text-md-right">
-                                        <a href="#">Forgot Password</a>
-                                    </div> -->
-                            </div>
-                            <div class="form-group">
-                                <button type="submit" class="btn btn-primary rounded submit p-3 px-5">
-                                    Login
-                                </button>
-                                <br>
-                                <br>
-                                <br>
-                                <a href="http://127.0.0.1:8000/register">Don't have an account? Create account</a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+<div class="container mt-5">
+  <div class="row justify-content-center">
+    <div class="col-md-6">
+      <div class="card">
+        <div class="card-header">
+          <h4>Login</h4>
         </div>
-    </section>
+        <div class="card-body">
+          <form action="/login" method="post">
+            @csrf <!-- Add this line if you are using Laravel to protect against CSRF attacks -->
+            <div class="form-group">
+              <label for="email">Email</label>
+              <input type="email" name="email" id="email" class="form-control" required>
+            </div>
+            <div class="form-group">
+              <label for="password">Password</label>
+              <input type="password" name="password" id="password" class="form-control" required>
+            </div>
+            <button type="submit" class="btn btn-primary">Login</button>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
 
-    <script src="js/jquery.min.js"></script>
-    <script src="js/popper.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
+<!-- Link to Bootstrap JS and jQuery (required for Bootstrap's JavaScript plugins) -->
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.1/dist/umd/popper.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
 </body>
-
 </html>
