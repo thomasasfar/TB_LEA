8caa3abe7d4b2e9c20f996fe9d9136258eba210b
najlandv <nadiva979@gmail.com>
2023-07-26 08:30:08 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -30,16 +30,3 @@ Route::get('/barang', [App\Http\Controllers\BarangController::class, 'index']);
 Route::post('/barang/tambah', [App\Http\Controllers\BarangController::class, 'store']);
 Route::delete('/barang/{barang}/hapus', [App\Http\Controllers\BarangController::class, 'destroy']);
 Route::put('/barang/{barang}/update', [App\Http\Controllers\BarangController::class, 'update']);
-<<<<<<< HEAD
- 
-
-<<<<<<< HEAD
-Route::get('/login',[AuthController::class,'login']);
-Route::get('/register',[AuthController::class,'register']);
-Route::get('/profile',[AuthController::class,'profile']);
-=======
->>>>>>> 907f72522e8016078494a4d06c4aa650b2f6b944
-=======
->>>>>>> e9a3b2efd41eded5103789f94393350361b0e4b6
-
-Route::get('/user/profile', [App\Http\Controllers\AuthController::class, 'profile']); 
