146ab554ddb7bce92ae2dd925bc6e60b1bce7a8a
Nadia Deari Hanifah <nadyadearihanifah@gmail.com>
2023-08-07 15:14:29 +0700

    .

--- resources/views/profile/password.blade.php
+++ resources/views/profile/password.blade.php
@@ -7,13 +7,56 @@
 @section('konten')
     @include('header')
 
-    <form action="/password/update" method="POST">
-    @csrf
-    @method('PUT')
-    <label for="new_password">Password Baru</label>
-    <input type="password" name="new_password" required>
-    <button type="submit">Ubah Password</button>
-</form>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Ubah Password</div>
+
+                    <div class="card-body">
+                        @if(session('success'))
+                            <div class="alert alert-success">{{ session('success') }}</div>
+                        @endif
+
+                        <form method="POST" action="{{ route('/profile/password') }}">
+                            @csrf
+                            @method('PUT')
+
+                            <div class="form-group row">
+                                <label for="new_password" class="col-md-4 col-form-label text-md-right">Password Baru</label>
+
+                                <div class="col-md-6">
+                                    <input id="new_password" type="password" class="form-control @error('new_password') is-invalid @enderror" name="new_password" required>
+
+                                    @error('new_password')
+                                        <span class="invalid-feedback" role="alert">
+                                            <strong>{{ $message }}</strong>
+                                        </span>
+                                    @enderror
+                                </div>
+                            </div>
+
+                            <div class="form-group row">
+                                <label for="new_password_confirmation" class="col-md-4 col-form-label text-md-right">Konfirmasi Password Baru</label>
+
+                                <div class="col-md-6">
+                                    <input id="new_password_confirmation" type="password" class="form-control" name="new_password_confirmation" required>
+                                </div>
+                            </div>
+
+                            <div class="form-group row mb-0">
+                                <div class="col-md-6 offset-md-4">
+                                    <button type="submit" class="btn btn-primary">
+                                        Ubah Password
+                                    </button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
 
