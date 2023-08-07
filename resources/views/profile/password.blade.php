148190dc0deb236c17b208f3a409192a70c19c12
Nadia Deari Hanifah <nadyadearihanifah@gmail.com>
2023-08-07 15:00:50 +0700

    .

--- resources/views/profile/password.blade.php
+++ resources/views/profile/password.blade.php
@@ -9,6 +9,7 @@
 
     <form action="/password/update" method="POST">
     @csrf
+    @method('PUT')
     <label for="new_password">Password Baru</label>
     <input type="password" name="new_password" required>
     <button type="submit">Ubah Password</button>
